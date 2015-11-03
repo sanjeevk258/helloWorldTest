08a56183d2a03de2894e172182bc68ebd2a017b9
rajesh_kumarvg <rajesh_kumarvg@BRL04354.persistent.co.in>
2015-11-03 11:34:00 +0530

    updating the project structure

---
+++ src/org/eclipse/swtbot/example/Activator.java
@@ -0,0 +1,54 @@
+package org.eclipse.swtbot.example;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.eclipse.swtbot.example"; //$NON-NLS-1$
+
+	// The shared instance
+	private static Activator plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		System.out.println("Test  j");
+		super.start(context);
+		plugin = this;
+		System.out.println("Test  g");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		System.out.println("Test  b");
+		plugin = null;
+		super.stop(context);
+		System.out.println("Test  a");
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+}
---
+++ src/org/eclipse/swtbot/example/Myfirsttest.java
@@ -0,0 +1,62 @@
+package org.eclipse.swtbot.example;
+
+import static org.junit.Assert.*;
+
+import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.eclipse.finder.waits.Conditions;
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(SWTBotJunit4ClassRunner.class)
+public class Myfirsttest {
+
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
+
+	private static SWTWorkbenchBot	bot;
+	 
+	@BeforeClass
+	public static void beforeClass() throws Exception {
+		bot = new SWTWorkbenchBot();
+		System.out.println("Test.............0");
+		bot.viewByTitle("Welcome").close();
+		System.out.println("Test.............010");
+	}
+ 
+ 
+	@Test
+	public void canCreateANewJavaProject() throws Exception {
+		bot.menu("File").menu("New").menu("Project...").click();
+ 
+		SWTBotShell shell = bot.shell("New Project");
+		shell.activate();
+		bot.tree().expandNode("Java").select("Java Project");
+		bot.button("Next >").click();
+		System.out.println("Test.............1");
+ 
+		bot.textWithLabel("Project name:").setText("MyFirstProject");
+        
+		bot.button("Finish").click();
+		bot.waitUntil(Conditions.shellIsActive("Open Associated Perspective?"));
+		bot.button("Yes").click();
+
+		System.out.println("Test.............2");
+		// FIXME: assert that the project is actually created, for later
+	}
+ 
+ 
+	@AfterClass
+	public static void sleep() {
+		System.out.println("Test");
+		bot.sleep(2000);
+		System.out.println("Testcompleted");
+	}
+
+}
