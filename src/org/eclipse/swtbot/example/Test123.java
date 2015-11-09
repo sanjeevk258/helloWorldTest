05548e7ff59323bf1d484a82347841c90922dbcf
Rajesh Kumar V G <rajesh_kumarvg@persistent.co.in>
2015-11-09 14:18:29 +0530

    New file Test123

---
+++ src/org/eclipse/swtbot/example/Test123.java
@@ -0,0 +1,14 @@
+package org.eclipse.swtbot.example;
+
+import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.junit.BeforeClass;
+
+public class Test123 {
+	@BeforeClass
+	public static void beforeClass() throws Exception {
+		
+		System.out.println("Test.............0");
+		System.out.println("Test.............010");
+	}
+ 
+}
